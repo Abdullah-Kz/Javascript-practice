dcca3ffbe686ae72ac65878bb2777d74e4c32f7b
Abdullah-Kz <romantech2@gmail.com>
2024-08-08 08:44:03 -0700

    operators and compare value concepts

---
+++ Learning/comapre.js
@@ -0,0 +1,25 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2!=1)
+// console.log(2==1)
+
+
+
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null>0)
+// console.log(null<=0)
+// console.log(null<0)
+// line 8 will be false because null is not a number it is a object
+// but in line 10 and 12 because of the greater and lesser than sign null will be treated as 0 but output will still be false because there is no ==
+// In line 9 and 11 outpout will be true because greater than and less than sign converts null into 0 and = compares it with 0
+
+
+// console.log("2" == 2)
+// console.log(true == 1)
+// console.log("02" == 2)
+// this will be true because node will convert "02" and 2 into 2
+
+console.log("2"==2)
+console.log("2"===2)
+//24th line will be false beacause === is strict equality it checks the value as we;; ad the  dayatype
--- Learning/operations.js
+++ Learning/operations.js
@@ -1,4 +1,16 @@
 let a = 5 
 let b = 10 
 console.log(a+b)
-console.log(a**b)
+// like that all the operations can be performed
+let str1 = 'Abdullah'
+let str2 = ' Khanzada'
+console.log(str1+str2)
+console.log("1"+2) //-> 12
+console.log(1+'3') //-> 13
+console.log("1"+2+2) //-> 122
+console.log(1+2+"2") //-> 32
+
+console.log(a++)//this will first print the number and then add it
+console.log(a)//when this will be printed the number will be 6 because in line 13 it was incremented using post increment 
+console.log(++a)//
+console.log(+true)
